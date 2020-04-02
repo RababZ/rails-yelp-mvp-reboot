1690d16e66cd07f0bedc8f5f16c7f3b3cc947dc4
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-02 21:07:25 +0100

    seeded restaurants and edited routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[Chinese Italian Japanese French Belgian].sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished"
