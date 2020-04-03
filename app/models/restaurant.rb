e70dabad8f44a15b77487c5d9ceab2de66e3ae40
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-03 15:16:08 +0100

    generated review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w(Chinese Italian Japanese French Belgian), message: "%{value} is not a valid size" }
+
+  has_many :reviews, dependent: :destroy
 end
